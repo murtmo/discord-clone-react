48a026a98f6f7f9822b06e43e11c97ac4f8ea168
TamaoMiura <miuratamao.d@gmail.com>
2024-06-02 21:30:15 +0900

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return <div className="home">home</div>;
+};
+
+export default Home;
