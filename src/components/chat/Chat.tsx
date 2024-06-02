72f73f7dd9faada92750063126017db60bd04324
TamaoMiura <miuratamao.d@gmail.com>
2024-06-02 23:17:57 +0900

    create chat area

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Chat.scss";
+
+import ChatHeader from "./ChatHeader";
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <ChatHeader />
+      <div className="chatMessage"></div>
+      <div className="chatInput"></div>
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Home.scss";
-
-const Home = () => {
-  return <div className="home">home</div>;
-};
-
-export default Home;
