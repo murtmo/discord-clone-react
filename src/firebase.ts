6812eb6ae008e2362fcd28b7dad2255f72ff89ef
TamaoMiura <miuratamao.d@gmail.com>
2024-06-05 00:03:46 +0900

    installed firebase and set firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClzT04mJfTv0q8S-PC038KAhyuWjNevgQ",
+  authDomain: "discord-clone-react-1c4eb.firebaseapp.com",
+  projectId: "discord-clone-react-1c4eb",
+  storageBucket: "discord-clone-react-1c4eb.appspot.com",
+  messagingSenderId: "587390071553",
+  appId: "1:587390071553:web:7270e7b21b177e8f096100",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
