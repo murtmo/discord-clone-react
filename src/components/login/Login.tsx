27f696a4d954ed263ef6c20cfc1dc0729e1d994c
TamaoMiura <miuratamao.d@gmail.com>
2024-06-06 00:24:03 +0900

    style adjustments

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Login.scss";
 
+import Face3Icon from "@mui/icons-material/Face3";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="loginLogo">
+        <Face3Icon />
+      </div>
+
+      <button>login</button>
+    </div>
+  );
 };
 
 export default Login;
