087b6bebf86602ab032b1ccb65ee83214bb30c9f
TamaoMiura <miuratamao.d@gmail.com>
2024-06-06 18:35:46 +0900

    call signInWithPopup to set a pop-up login window

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import "./Login.scss";
 
 import Face3Icon from "@mui/icons-material/Face3";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../firebase";
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
   return (
     <div className="login">
       <div className="loginLogo">
         <Face3Icon />
       </div>
 
-      <button>login</button>
+      <button onClick={signIn}>login</button>
     </div>
   );
 };
