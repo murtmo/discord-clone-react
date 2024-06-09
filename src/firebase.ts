99c3542e7ce01c4243eed070c2ee7ea1b8efc3e0
TamaoMiura <miuratamao.d@gmail.com>
2024-06-09 21:56:10 +0900

    hide api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClzT04mJfTv0q8S-PC038KAhyuWjNevgQ",
-  authDomain: "discord-clone-react-1c4eb.firebaseapp.com",
-  projectId: "discord-clone-react-1c4eb",
-  storageBucket: "discord-clone-react-1c4eb.appspot.com",
-  messagingSenderId: "587390071553",
-  appId: "1:587390071553:web:7270e7b21b177e8f096100",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
